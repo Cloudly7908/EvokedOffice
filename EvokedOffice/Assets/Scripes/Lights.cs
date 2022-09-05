f769e7dfd248816d45c7c91ad89a4cb3341750a9
Cloudly7908 <stangline23632@gmail.com>
2022-09-05 11:52:50 -0400

    yuifcyre

--- EvokedOffice/Assets/Scripes/Lights.cs
+++ EvokedOffice/Assets/Scripes/Lights.cs
@@ -1,14 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Lights : MonoBehaviour
 {
  
     [SerializeField] private Material myMaterial;
+
+    [SerializeField] private float timeMultiplier;
+
+    [SerializeField] private float startHour;
+
+    public float LighsOFF;
+
+    public float LighsON;
+
+    [SerializeField] private TextMeshProUGUI timeText;
+
+
+    private DateTime currentTime;
+
+
+    void Start()
+    {
+        Color on = new Color(0.775f, 0.806f, 0.904f, 1);
+        myMaterial.color = on; ;
+        myMaterial.SetColor("_EmissionColor", on);
+        RenderSettings.ambientLight = on;
+        Debug.Log("LightsOn");
+
+        currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour);
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        UpdateTimeOfDay();
+    }
+
+    private void UpdateTimeOfDay()
+    {
+        currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultiplier);
+
+        if (timeText != null)
+        {
+            timeText.text = currentTime.ToString("HH:mm");
+        }
+        Debug.Log(timeText.text);
+
+        if (currentTime.Hour == LighsOFF)
         {
             Color off = new Color(0.0849f, 0.0896f, 0.1132f, 1);
             Color offoff = new Color(0.06f, 0.06f, 0.06f, 1);
@@ -16,16 +57,16 @@ public class Lights : MonoBehaviour
             myMaterial.SetColor("_EmissionColor", off);
             RenderSettings.ambientLight = offoff;
             Debug.Log("LightsOff");
-
         }
 
-        if (Input.GetKeyDown(KeyCode.O))
+        if (currentTime.Hour == LighsON)
         {
             Color on = new Color(0.775f, 0.806f, 0.904f, 1);
-            myMaterial.color = on;;
+            myMaterial.color = on; ;
             myMaterial.SetColor("_EmissionColor", on);
             RenderSettings.ambientLight = on;
             Debug.Log("LightsOn");
         }
+
     }
 }
