3b363e9d714d46d00f466eef59ef0c1f5c4fd8c7
Cloudly <93558974+Cloudly7908@users.noreply.github.com>
2022-09-27 20:33:18 -0400

    state
    fs

---
+++ EvokedOffice/Assets/Scripes/NavigationBaker.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationBaker : MonoBehaviour {
+
+    public NavMeshSurface[] surfaces;
+    public Transform[] objectsToRotate;
+
+    // Use this for initialization
+    void Awake() 
+    {
+
+        for (int i = 0; i < surfaces.Length; i++) 
+        {
+            surfaces [i].BuildNavMesh ();    
+        }    
+    }
+
+}
